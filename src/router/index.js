2e2f3ddf4d71812b3a709a969e9bd403dd257cce
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2022-08-05 19:15:02 +0530

    ADD: routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,39 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Welcome from "../views/Welcome.vue";
+import About from "../views/About.vue";
+import Summary from "../views/Summary.vue";
+import AgeError from "../views/AgeError.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Welcome",
+    component: Welcome,
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    component: About,
+  },
+  {
+    path: "/summary",
+    name: "Summary",
+    component: Summary,
+  },
+  {
+    path: "/age-error",
+    name: "AgeError",
+    component: AgeError,
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
