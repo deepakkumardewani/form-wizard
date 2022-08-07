1ad0ab07eb73ad4e6829a9e1f6a71bcab9842b0a
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2022-08-07 08:30:35 +0530

    ADD: table layout for summary

--- src/views/Summary.vue
+++ src/views/Summary.vue
@@ -1,25 +1,83 @@
 <template>
-  <div class="summary-container mx-auto p-14">
-    <p class="font-bold text-black text-3xl my-4">Summary</p>
-    <p class="my-6">Name: {{data.name}}</p>
-    <p class="my-6">Age: {{data.age}}</p>
-    <p class="my-6">Where do you live: {{data.country}}</p>
-    <p class="my-6">Package: {{data.packageSelected.name}}</p>
-    <p class="my-6">Premium: {{data.premium}}</p>
+  <div class="flex h-screen justify-center items-center">
+    <div class="summary-container mx-auto p-14 shadow-2xl shadow-black ">
+      <p class="font-bold text-black text-3xl my-4">Summary</p>
 
-    <div class="mt-5 inline-flex">
-      <button
-        @click="back()"
+      <table class="w-full text-sm text-left text-gray-500  border-collapse border border-gray-300">
+
+        <tbody>
+          <tr class="border-b border-gray-300">
+            <th
+              scope="row"
+              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Name
+            </th>
+            <td class="py-4 px-6">
+              {{data.name}}
+            </td>
+          </tr>
+          <tr class="border-b  border-gray-300">
+            <th
+              scope="row"
+              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Age
+            </th>
+            <td class="py-4 px-6">
+              {{data.age}}
+            </td>
+          </tr>
+          <tr class="border-b  border-gray-300">
+            <th
+              scope="row"
+              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Where do you live?
+            </th>
+            <td class="py-4 px-6">
+              {{data.country}}
+            </td>
+          </tr>
+          <tr class="border-b  border-gray-300">
+            <th
+              scope="row"
+              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Package
+            </th>
+            <td class="py-4 px-6">
+              {{data.packageSelected.name}}
+            </td>
+          </tr>
+          <tr class="border-b  border-gray-300">
+            <th
+              scope="row"
+              class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Premium
+            </th>
+            <td class="py-4 px-6">
+              {{data.premium}}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div class="mt-5 inline-flex">
+        <button
+          @click="back()"
           class="bg-white hover:bg-gray-800 hover:text-white transition mr-4 border border-black text-black py-2 px-12 rounded"
-      >
-        Back
-      </button>
-      <button
-        @click="buy()"
+        >
+          Back
+        </button>
+        <button
+          @click="buy()"
           class="bg-blue-500 hover:bg-blue-700 transition border border-blue-900 text-white py-2 px-12 rounded"
-      >
-        Buy
-      </button>
+        >
+          Buy
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -50,7 +108,7 @@ export default {
 <style>
 .summary-container {
   width: 50%;
-  height: 50%;
+  /* height: 50%; */
   background: rgb(244 244 244);
 }
 </style>
